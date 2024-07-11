d8f454917754ffaeae6865403ba6dcb904835c08
MunasaJyothi <jyothimunasa445@gmail.com>
2024-07-11 16:57:34 +0530

    done

---
+++ Addition1.java
@@ -0,0 +1,10 @@
+class Addition1
+{
+public static void main(String [] args)
+{
+int n1=20;
+int n2=10;
+int add=n1+n2;
+System.out.println("Addition of two numbers"+add);
+}
+}
---
+++ Even1.java
@@ -0,0 +1,16 @@
+class Even1
+{
+public static void main(String[] args)
+{
+int n1=10;
+if(n1 %==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.ptintln("Odd");
+
+}
+}
+}
